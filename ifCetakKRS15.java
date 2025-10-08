8f6b35fd77b4b7b489a24cf4a9bb9951c3922fbb
Madfar06 <gfrnrl19@gmail.com>
2025-10-08 20:00:22 +0700

    Update ifCetakKRS15.java

--- ifCetakKRS15.java
+++ ifCetakKRS15.java
@@ -7,11 +7,13 @@ public class ifCetakKRS15 {
         System.out.println("---cetak KRS SIAKAD---");
         System.out.print("apakah UKT sudah lunas (true/false): ");
         boolean uktLunas = sc.nextBoolean();
+        
 
         if (uktLunas) {
             System.out.println("pembayaran UKT terverifikasi");
             System.out.println("silakan cetak KRS dan minta tanda tangan DPA");
-            
+           }
+           else  {System.out.println("Register di tolak. Silakan lunasi UKT terlebih dahulu.");}
+           scanner.close();
         }
-    }
-}
+} 
