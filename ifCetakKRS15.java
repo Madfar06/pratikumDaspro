e48b97c5733169df1e7f6b4055e5bba3dcb75215
Madfar06 <gfrnrl19@gmail.com>
2025-10-08 08:01:46 +0700

    modifikasi percobaan 1

---
+++ ifCetakKRS15.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ifCetakKRS15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("---cetak KRS SIAKAD---");
+        System.out.print("apakah UKT sudah lunas (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("pembayaran UKT terverifikasi");
+            System.out.println("silakan cetak KRS dan minta tanda tangan DPA");
+            
+        }
+    }
+}
