de64915dd62a019af0aad9cc9d09a5d67110f9c4
Madfar06 <gfrnrl19@gmail.com>
2025-10-23 12:47:16 +0700

    percobaan 1

--- siakadFor15.java
+++ siakadFor15.java
@@ -4,9 +4,8 @@ public class siakadFor15 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    double nilai, tertinggi = 0, terrendah = 100, nilaiMinimum = 60;
-    int lulus = 0, tidakLulus = 0;
-
+    double nilai, tertinggi = 0, terrendah = 100;
+  
     for ( int i = 1 ; i <= 10; i++ ) {
         System.out.println("Masukan nilai mahasiswa ke-" + i + " : ");
         nilai = sc.nextDouble();
@@ -16,18 +15,11 @@ public class siakadFor15 {
         if (nilai < terrendah) {
             terrendah = nilai;
         }
-
-        if ( nilai >=  nilaiMinimum ) {
-            lulus++;
-        } else {
-            tidakLulus++;
-        }
        
      }
         System.out.println("nilai tertinggi: " + tertinggi);
         System.out.println("nilai terrendah : " + terrendah);
-        System.out.println("jumlah mahasiswa yang lulus " + lulus + " orang");
-        System.out.println("jumlah mahasiswa yang tidak lulus " + tidakLulus + " orang");
+    
     }
 }
 
