533969e440522339412a3d7babdf415b401d97b5
Madfar06 <gfrnrl19@gmail.com>
2025-10-30 06:09:35 +0700

    percobaan 1

--- siakadFor15.java
+++ siakadFor15.java
@@ -2,24 +2,23 @@ import java.util.Scanner;
 
 public class siakadFor15 {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner (System.in);
 
-    double nilai, tertinggi = 0, terrendah = 100;
-  
-    for ( int i = 1 ; i <= 10; i++ ) {
-        System.out.println("Masukan nilai mahasiswa ke-" + i + " : ");
-        nilai = sc.nextDouble();
-        if (nilai > tertinggi ) {
-            tertinggi = nilai;
+        double nilai, tertinggi = 0, terendah = 100;
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+            
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+            
+
         }
-        if (nilai < terrendah) {
-            terrendah = nilai;
-        }
-       
-     }
-        System.out.println("nilai tertinggi: " + tertinggi);
-        System.out.println("nilai terrendah : " + terrendah);
-    
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
     }
 }
-
