f2089060dc3b3b7f609c00608cf8403de1668a52
Madfar06 <gfrnrl19@gmail.com>
2025-11-23 14:12:22 +0700

    percobaan 3

---
+++ jobsheet10/number15.java
@@ -0,0 +1,12 @@
+
+public class number15 {
+    public static void main(String[] args) {
+       
+        int[][] myNumbers = new int[3][]; 
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+
+       
+    }
+}
