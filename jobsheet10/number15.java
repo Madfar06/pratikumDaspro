7f23f10b0401745e9dfe6b32726f00f5a7e9abcd
Madfar06 <gfrnrl19@gmail.com>
2025-11-23 14:12:52 +0700

    modifikasi percobaan 3

--- jobsheet10/number15.java
+++ jobsheet10/number15.java
@@ -1,3 +1,4 @@
+import java.util.Arrays; 
 
 public class number15 {
     public static void main(String[] args) {
@@ -7,6 +8,12 @@ public class number15 {
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
 
-       
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i])); 
+        }
+
+        for (int i = 0; i < myNumbers.length; i++) { 
+            System.out.println("panjang baris ke-" + (i + 1) + " : " + myNumbers[i].length);
+        }
     }
 }
