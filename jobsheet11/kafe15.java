08514fc74c9f1c17ef61e8fe8a333ff32549ab85
Madfar06 <gfrnrl19@gmail.com>
2025-12-09 20:34:42 +0700

    percobaan 1

---
+++ jobsheet11/kafe15.java
@@ -0,0 +1,18 @@
+package jobsheet11;
+
+public class kafe15 {
+    public static void menu(){
+     System.out.println("====== Menu Resto Kafe ========");   
+     System.out.println("1. kopi hitam - Rp. 15,000");   
+     System.out.println("2. cappucino - RP. 20,000");   
+     System.out.println("3. latte - Rp. 22,000");   
+     System.out.println("4. teh tarik - Rp. 12,000");   
+     System.out.println("5. roti bakar - Rp. 10,000");   
+     System.out.println("6. mie goreng - Rp. 18,000");   
+     System.out.println("==================================");   
+     System.out.println("silakan pilih menu yang anda inginkan");   
+    }
+    public static void main(String[] args) {
+    menu();
+    }
+}
